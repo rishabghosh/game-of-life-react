eabf810857f8663934558933e7e4305231381427
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-03 17:23:27 +0530

    create Cells which change state and color when clicked

--- src/App.js
+++ src/App.js
@@ -1,108 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./App.css";
+import nextGeneration from "./golLib.js";
 
-const BOUNDS = { topLeft: [1, 1], bottomRight: [4, 4] };
+const COLOR = { marked: "black", unmarked: "white" };
 
-const CURRENT_GENERATION = [
-  [1, 2],
-  [1, 3],
-  [2, 1],
-  [2, 4],
-  [3, 2],
-  [3, 4],
-  [4, 3]
-];
+const Cell = function() {
+  const [cellState, setCellState] = useState(false);
+  const [color, setCellColor] = useState(COLOR.unmarked);
 
-const CELL_HEIGHT_IN_PX = 20;
-const CELL_WIDTH_IN_PX = 20;
-
-const inPixel = (arg) => arg + "px";
-
-const styleCell = function(){
-  const height = inPixel(CELL_HEIGHT_IN_PX);
-  const width = inPixel(CELL_WIDTH_IN_PX);
-  const style  = {height ,width};
-  return style;
-}
-
-const styleRow = function(){
-  const height = inPixel(CELL_HEIGHT_IN_PX);
-  const width  = inPixel(CELL_WIDTH_IN_PX * getDimension(BOUNDS).width);
-  const style = { height , width};
-  return style;
-}
-
-const getDimension = function(bounds) {
-  const { topLeft, bottomRight } = bounds;
-  const height = bottomRight[0] - topLeft[0];
-  const width = bottomRight[1] - topLeft[0];
-  const dimension = { height, width };
-  return dimension;
+  const toggleState = () => {
+    setCellState(!cellState);
+    if (color === COLOR.marked) {
+      setCellColor(COLOR.unmarked);
+    } else {
+      setCellColor(COLOR.marked);
+    }
+  };
+  console.log("cell state is ", cellState);
+  console.log("cell color is ", color);
+  return <button style={{background: color}} onClick={toggleState} />;
 };
 
-const getWidth = function(bounds) {
-  const { width } = getDimension(bounds);
-  return new Array(width).fill(0);
+const Row = function() {
+  const result = [];
+  for (let index = 0; index < 10; index++) {
+    result.push(<Cell />);
+  }
+  return <div className="row">{result}</div>;
 };
 
-const getHeight = function(bounds) {
-  const { height } = getDimension(bounds);
-  return new Array(height).fill(0);
+const Table = function() {
+  const result = [];
+  for (let index = 0; index < 10; index++) {
+    result.push(<Row />);
+  }
+  return <div className="table">{result}</div>;
 };
 
-class Cell extends Component {
-  render() {
-    return <div style={styleCell()} className="cell" />;
-  }
-}
-
-class Row extends Component {
-  constructor(props) {
-    super(props);
-    this.width = getWidth(BOUNDS);
-  }
-
-  render() {
-    return (
-      <div className="row" style={styleRow()}>
-        {this.width.map(cell => (
-          <Cell />
-        ))}
-      </div>
-    );
-  }
-}
-
-class Field extends Component {
-  constructor(props) {
-    super(props);
-    this.height = getHeight(BOUNDS);
-  }
-  render() {
-    return (
-      <div className="field">
-        {this.height.map(row => (
-          <Row />
-        ))}
-      </div>
-    );
-  }
-}
-
-class ViewPort extends Component {
-  render() {
-    return (
-      <main>
-        <Field />
-      </main>
-    );
-  }
-}
-
-class App extends Component {
-  render() {
-    return <ViewPort />;
-  }
-}
+const App = function() {
+  return (
+    <main>
+      <Table />
+    </main>
+  );
+};
 
 export default App;
